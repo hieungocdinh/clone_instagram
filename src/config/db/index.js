81e02d24243a0690fefd61047887370400468dae
Đinh Ngọc Hiếu <91507601+hieungocdinh@users.noreply.github.com>
2024-06-30 10:48:17 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
+        // Đặt tùy chọn strictQuery
+        mongoose.set('strictQuery', false);
+        
         await mongoose.connect(process.env.MONGO_URI, {
             //useNewUrlParser: true,
             //useUnifiedTopology: true,
