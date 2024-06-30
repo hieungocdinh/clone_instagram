08ff22bc36bdf169b140b1255a287dcb7f489e3b
hieungocdinh <hieungocdinh2003@gmail.com>
2024-06-30 10:08:53 +0700

    update mongo url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 const route = require('./routes');
 const db = require('./config/db');
