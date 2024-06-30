5f4e3b4bd411415cccf5549fe89d4423184aac59
hieungocdinh <hieungocdinh2003@gmail.com>
2024-06-30 10:31:12 +0700

    final 30/6/2024

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 async function connect() {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log('Connect successfully!!!');
     } catch (error) {
